2c5ae6584e2c77f684a2a163736b91fc8d2f1793
Kyy <mynev.id@gmail.com>
2022-04-11 02:38:28 +0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -28,7 +28,17 @@ export default function Document() {
       </Head>
       <body>
         <Main />
-        <NextScript />
+ {
+  process.env.NODE_ENV === "production" && (
+    <script
+      async
+      defer
+      data-website-id="1656c6d6-c8e0-4352-bb7a-ef1538170dc3"
+      src="https://umami-production-e27e.up.railway.app/umami.js"
+    ></script>
+  );
+}
+      <NextScript />
       </body>
     </Html>
   );
