5a2a0a55006ccc067700463426f02e8dc9f14112
ioofy <mynev.id@gmail.com>
2022-04-24 12:10:14 +0800

    feat: change sidemenu animation

--- src/layout/components/Navbar/Burger/Burger.tsx
+++ src/layout/components/Navbar/Burger/Burger.tsx
@@ -1,142 +1,57 @@
-import React, { useState } from "react";
-import styled from "@emotion/styled";
-import SideMenu from "../SideMenu/MenuItem";
+import React from "react";
+import cn from "classnames";
+import { motion } from "framer-motion";
+import { css } from "@emotion/css";
 
-type StateProps = {
-  initial?: boolean;
-  clicked?: boolean;
-  menuName?: string;
-};
+const Path = (props: any) => (
+  <motion.path
+    fill="transparent"
+    strokeWidth="3"
+    stroke="#faf9f9"
+    strokeLinecap="round"
+    {...props}
+  />
+);
 
-type LineProps = {
-  isActive: boolean;
-};
-
-const Wrapper = styled.div`
-  margin-right: 14px;
-  display: none;
-  z-index: 2;
-
-  #hamburger {
-    -webkit-transition: all 0.3s ease-in-out;
-    -o-transition: all 0.3s ease-in-out;
-    transition: all 0.3s ease-in-out;
-  }
-  #hamburger.is-active {
-    -webkit-transition: all 0.3s ease-in-out;
-    -o-transition: all 0.3s ease-in-out;
-    transition: all 0.3s ease-in-out;
-    -webkit-transition-delay: 0.6s;
-    -o-transition-delay: 0.6s;
-    transition-delay: 0.6s;
-    -webkit-transform: rotate(45deg);
-    -ms-transform: rotate(45deg);
-    -o-transform: rotate(45deg);
-    transform: rotate(45deg);
-  }
-  #hamburger.is-active .__line:nth-of-type(2) {
-    width: 0px;
-  }
-
-  #hamburger.is-active .__line:nth-of-type(1),
-  #hamburger.is-active .__line:nth-of-type(3) {
-    -webkit-transition-delay: 0.3s;
-    -o-transition-delay: 0.3s;
-    transition-delay: 0.3s;
-  }
-  #hamburger.is-active .__line:nth-of-type(1) {
-    -webkit-transform: translateY(13px);
-    -ms-transform: translateY(13px);
-    -o-transform: translateY(13px);
-    transform: translateY(13px);
-  }
-  #hamburger.is-active .__line:nth-of-type(3) {
-    -webkit-transform: translateY(-13px) rotate(90deg);
-    -ms-transform: translateY(-13px) rotate(90deg);
-    -o-transform: translateY(-13px) rotate(90deg);
-    transform: translateY(-13px) rotate(90deg);
-    margin-top: 13px;
-  }
-  @media screen and (max-width: 653px) {
-    display: block;
-  }
-`;
-
-const Hamburger = styled.button`
-  outline: none;
-  border: none;
-  background: transparent;
-`;
-
-const Line = styled.span<LineProps>`
-  width: 35px;
-  height: 2px;
-  right: 20px;
-  background-color: #1bd6ca;
-  display: block;
-  margin: ${({ isActive }) => (isActive ? "10px" : "8px")};
-  -webkit-transition: all 0.3s ease-in-out;
-  -o-transition: all 0.3s ease-in-out;
-  transition: all 0.3s ease-in-out;
-`;
-
-const Burger = () => {
-  const [state, setState] = useState<StateProps>({
-    initial: false,
-    clicked: null,
-    menuName: "Menu",
-  });
-
-  const [disabled, setDisabled] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleMenu = () => {
-    setIsOpen(!isOpen);
-    disableMenu();
-
-    if (state.initial === false) {
-      setState({
-        initial: null,
-        clicked: true,
-        menuName: "Close",
-      });
-    } else if (state.clicked === true) {
-      setState({
-        clicked: !state.clicked,
-        menuName: "Close",
-      });
-    } else if (state.clicked === false) {
-      setState({
-        clicked: !state.clicked,
-        menuName: "Close",
-      });
-    }
-  };
-
-  const disableMenu = () => {
-    setDisabled(!disabled);
-
-    setTimeout(() => {
-      setDisabled(false);
-    }, 1300);
-  };
-
-  return (
-    <Wrapper>
-      <Hamburger
-        id="hamburger"
-        disabled={disabled}
-        className={isOpen ? `is-active` : `not-active`}
-        onClick={handleMenu}
-        type="button"
-      >
-        <Line className="__line" isActive={isOpen}></Line>
-        <Line className="__line" isActive={isOpen}></Line>
-        <Line className="__line" isActive={isOpen}></Line>
-      </Hamburger>
-      <SideMenu isOpened={isOpen} />
-    </Wrapper>
-  );
-};
-
-export default Burger;
+export const Burger = ({ toggle }: any) => (
+  <button
+    onClick={toggle}
+    className={cn(
+      "outline-none border-none bg-transparent cursor-pointer absolute hidden sm:block",
+      css`
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        top: 18px;
+        left: 15px;
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+      `,
+    )}
+    type="button"
+  >
+    <svg width="27" height="27" viewBox="0 0 23 23">
+      <Path
+        variants={{
+          closed: { d: "M 2 2.5 L 20 2.5" },
+          open: { d: "M 3 16.5 L 17 2.5" },
+        }}
+      />
+      <Path
+        d="M 2 9.423 L 20 9.423"
+        variants={{
+          closed: { opacity: 1 },
+          open: { opacity: 0 },
+        }}
+        transition={{ duration: 0.1 }}
+      />
+      <Path
+        variants={{
+          closed: { d: "M 2 16.346 L 20 16.346" },
+          open: { d: "M 3 2.5 L 17 16.346" },
+        }}
+      />
+    </svg>
+  </button>
+);
--- src/layout/components/Navbar/SideMenu/MenuItem.tsx
+++ src/layout/components/Navbar/SideMenu/MenuItem.tsx
@@ -1,87 +1,24 @@
 import React from "react";
-import styled from "@emotion/styled";
 import Link from "next/link";
-import NowPlaying from "@components/NowPlaying/NowPlaying";
-import { menuItem } from "../constant/constant";
+import { motion } from "framer-motion";
+import { livariants } from "@animation/animations";
 
-type SideMenuProps = {
-  isOpened: boolean;
+type MenuItemProps = {
+  name: string;
+  href: string | any;
 };
 
-const Wrapper = styled.div`
-  display: none;
-
-  @media screen and (max-width: 653px) {
-    display: block;
-  }
-`;
-
-const SideMenus = styled.ul<SideMenuProps>`
-  display: flex;
-  align-items: center;
-  z-index: -1;
-  list-style: none;
-  font-size: 1.1rem;
-
-  @media screen and (max-width: 653px) {
-    flex-flow: column nowrap;
-    align-items: flex-start;
-    background: #0d121c;
-    top: 0;
-    right: 0;
-    position: fixed;
-    transform: ${({ isOpened }) =>
-      isOpened ? "translateX(0)" : "translateX(100%)"};
-    height: 100vh;
-    width: 100%;
-    transition: transform 0.3s ease-in-out;
-  }
-`;
-
-const SideItems = styled.li`
-  display: block;
-  display: flex;
-  margin: 120px 0px;
-  padding: 40px;
-  flex-direction: column;
-`;
-
-const SideItemsWrapper = styled.div`
-  margin: 20px 0px;
-`;
-
-const LinkItems = styled.a`
-  text-decoration: none;
-  font-family: "Grotesk", sans-serif;
-  font-size: 2rem;
-  color: #fff;
-`;
-
-const SpotifyWrapper = styled.div`
-  bottom: 0;
-  position: absolute;
-  margin-bottom: 30px;
-`;
-
-const SideMenu: React.FC<SideMenuProps> = ({ isOpened }) => {
+export const MenuItem: React.FC<MenuItemProps> = ({ name, href }) => {
   return (
-    <Wrapper>
-      <SideMenus isOpened={isOpened}>
-        <SideItems>
-          {menuItem.map(({ href, id, name }) => (
-            <SideItemsWrapper key={id}>
-              <Link href={href}>
-                <LinkItems>{name}</LinkItems>
-              </Link>
-            </SideItemsWrapper>
-          ))}
-        </SideItems>
-        <SpotifyWrapper>
-          <NowPlaying />
-        </SpotifyWrapper>
-      </SideMenus>
-    </Wrapper>
+    <motion.li
+      variants={livariants}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.95 }}
+      className="hidden sm:block font-serif text-4xl py-5 px-1"
+    >
+      <Link href={href}>
+        <a className="no-underline">{name}</a>
+      </Link>
+    </motion.li>
   );
 };
-
-export default SideMenu;
