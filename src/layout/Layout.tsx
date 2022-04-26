2a1733e510fe1088b931aff4810dee17e6cf2dd5
ioofy <mynev.id@gmail.com>
2022-04-26 12:45:28 +0700

    fix: error on build runtime

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import styled from "@emotion/styled";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
+interface LayoutProps {
+  children: ReactNode;
+}
 
 const Main = styled.main`
   margin: 20px 0px;
 `;
 
-const Layout = (children: React.ReactNode) => {
+const Layout = (props: LayoutProps) => {
   return (
     <React.Fragment>
       <Navbar />
-      <Main>{children}</Main>
+      <Main>{props.children}</Main>
       <Footer />
     </React.Fragment>
   );
