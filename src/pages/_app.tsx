162f47519b2d275ba5826245f9fe7c28c14228d7
ioofy <mynev.id@gmail.com>
2022-07-16 16:41:24 +0700

    fix: build CI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@styles/css/index.css";
 import React from "react";
 import Layout from "@layout/Layout";
-import ThemeProviders from "@config/ThemeProvider";
+import ThemeProviders from "@config/ThemeProviders";
 import { Global } from "@emotion/react";
 import { globalStyles } from "@styles/global.styles";
 import { AppProps } from "next/app";
